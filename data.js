221ae74fc30776dc30a06b92666ba3201381a6bb
dalonso <masdavidalonso@gmail.com>
2025-07-10 15:38:26 +0200

    Fixed indentation issue from importing from Scrimba IDE

--- data.js
+++ data.js
@@ -1,25 +1,25 @@
 const menuArray = [
-    {
-        name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
-        id: 0,
-        price: 14,
-        emoji: "🍕"
-    },
-    {
-        name: "Hamburger",
-        ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
-        emoji: "🍔",
-        id: 1
-    },
-        {
-        name: "Beer",
-        ingredients: ["grain, hops, yeast, water"],
-        price: 12,
-        emoji: "🍺",
-        id: 2
-    }
+   {
+      name: "Pizza",
+      ingredients: ["pepperoni", "cheese", "mozarella"],
+      price: 14,
+      emoji: "🍕",
+      id: 0
+   },
+   {
+      name: "Hamburger",
+      ingredients: ["beef", "cheese", "lettuce", "tomato"],
+      price: 12,
+      emoji: "🍔",
+      id: 1
+   },
+      {
+      name: "Beer",
+      ingredients: ["grain, hops, yeast, water"],
+      price: 12,
+      emoji: "🍺",
+      id: 2
+   }
 ]
 
 export default menuArray
